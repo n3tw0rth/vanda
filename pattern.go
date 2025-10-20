f60b461f0f199b7af02b03abc469419b404dddab
n3tw0rth <rtheekshanafernando@gmail.com>
2025-10-20 21:44:44 +0530

    wip: keyworded params

--- pattern.go
+++ pattern.go
@@ -11,15 +11,25 @@ func ParsePattern(pattern string) (*CommandPattern, error) {
 		return nil, fmt.Errorf("empty pattern")
 	}
 
-	// first part must be <command>
+	// Process subcommands
 	if !strings.HasPrefix(parts[0], "<") || !strings.HasSuffix(parts[0], ">") {
+		//TODO: when no subcommands are defined, just follow a single pattern
 		return nil, fmt.Errorf("pattern must start with <command>")
 	}
+
 	cmdName := strings.Trim(parts[0], "<>")
 
 	var args []ArgPattern
 
-	for _, part := range parts[1:] {
+	var flags []string
+
+	if cmdName != "" {
+		flags = parts[1:]
+	} else {
+		flags = parts
+	}
+
+	for _, part := range flags {
 		switch {
 		// required argument
 		case strings.HasPrefix(part, "[") && strings.HasSuffix(part, "]"):
@@ -38,30 +48,27 @@ func ParsePattern(pattern string) (*CommandPattern, error) {
 		case strings.HasPrefix(part, "{") && strings.HasSuffix(part, "}"):
 			content := strings.Trim(part, "{}")
 
+			var nameType []string
 			// flag with value: {=flag:type}
-			if strings.HasPrefix(content, "=") {
-				content = strings.TrimPrefix(content, "=")
-				nameType := strings.Split(content, ":")
+			if cntn, ok := strings.CutPrefix(content, "="); ok {
+				nameType = strings.Split(cntn, ":")
 				if len(nameType) != 2 {
 					return nil, fmt.Errorf("invalid flag with value: %s", part)
 				}
-				args = append(args, ArgPattern{
-					Name: nameType[0],
-					Flag: "-" + nameType[0],
-					Type: ArgType(nameType[1]),
-				})
 			} else {
 				// boolean flag: {flag:bool}
-				nameType := strings.Split(content, ":")
+				nameType = strings.Split(cntn, ":")
 				if len(nameType) != 2 {
 					return nil, fmt.Errorf("invalid flag: %s", part)
 				}
-				args = append(args, ArgPattern{
-					Name: nameType[0],
-					Flag: "-" + nameType[0],
-					Type: ArgType(nameType[1]),
-				})
 			}
+
+			args = append(args, ArgPattern{
+				Name: nameType[0],
+				Flag: "-" + nameType[0],
+				Type: ArgType(nameType[1]),
+			})
+
 		}
 	}
 
